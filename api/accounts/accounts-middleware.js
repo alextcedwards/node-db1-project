52af3cfc8c47920dbcba3d68ad6c60ea97243993
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-07-12 21:59:48 -0600

    finished mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,61 @@
+const Account = require("./accounts-model");
+const db = require("../../data/db-config");
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const error = { status: 400 };
+  const { name, budget } = req.body;
+  // - If either name or budget are undefined, return `{ message: "name and budget are required" }`
+  if (name === undefined || budget === undefined) {
+    error.message = "name and budget are required";
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+    // - If name is not a string, return `{ message: "name of account must be a string" }`
+  } else if (typeof name !== "string") {
+    error.message = "name of account must be a string";
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+    // - If the _trimmed_ name is shorter than 3 or longer than 100, return `{ message: "name of account must be between 3 and 100" }`
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100";
+
+    // - If budget is not a number, return `{ message: "budget of account must be a number" }`
+  } else if (typeof budget !== "number" || isNaN(budget)) {
+    error.message = "budget of account must be a number";
+
+    // - If budget is a negative number or over one million, return  `{ message: "budget of account is too large or too small" }`
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = "budget of account is too large or too small";
+  }
+  if (error.message) {
+    next(error);
+  } else {
+    next();
+  }
+};
+
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db("accounts")
+      .where("name", req.body.name.trim())
+      .first();
+    if (existing) {
+      next({ status: 400, message: "that name is taken" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    if (!account) {
+      next({ status: 404, message: "account not found" });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,32 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  //select * from accounts;
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  //select * from accounts where id = 1;
+  return db("accounts").where("id", id).first();
+};
 
-const create = account => {
-  // DO YOUR MAGIC
-}
+const create = async (account) => {
+  //insert into accounts (name, budget) values ('foo', 1000);
+  const [id] = await db("accounts").insert(account);
+  return getById(id);
+};
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
+const updateById = async (id, account) => {
+  // update accounts set name = 'foo', budget = 1000 where id = 1; 
+  await db("accounts").where("id", id).update(account);
+  return getById(id);
+};
 
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const deleteById = (id) => {
+  // delete from accounts where id = 1;
+  return db("accounts").where("id", id).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +34,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
